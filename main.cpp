e53a11e229d1799afc1396b720c91c0829c957c3
daniel13man <daniel.mansouryan-tabaei@skola.uppsala.se>
2025-11-22 10:58:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
-
-using namespace std;
+#include <random>
 
 int main()
 {
-    int x;
-    int y;
-    int z;
-    int a;
-    cout << "enter a number: ";
-    cin >> x;
-    cout << "enter another nubmer: ";
-    cin >> y;
-    z = max(x, y);
-    cout << "the biggest number is: " << z << endl;
-    a = min(x, y);
-    cout << "the smallest number is: " << a << endl;
+   
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(1, 1000);
+
+    int question1 = dist(gen);
+    int question2 = dist(gen);
+    int answer;
+
+    std::cout << "What is " << question1 << " + " << question2 << "?" << std::endl;
+    if (!(std::cin >> answer)) {
+        std::cerr << "Invalid input. Please enter an integer." << std::endl;
+        return 1;
+    }
+
+    if (answer == question1 + question2) {
+        std::cout << "Correct!" << std::endl;
+    } else {
+        std::cout << "Incorrect. The correct answer is " << (question1 + question2) << "." << std::endl;
+    }
     return 0;
 }
+
 
 
